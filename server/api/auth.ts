b0cf5ac450c939f48757077a8cc086f48b429fda
kalpadhwaryu <kalpadhwaryu1581@gmail.com>
2024-10-07 15:38:34 +0530

    Add /me route to get user and workspace info & use it in frontend

--- server/api/auth.ts
+++ server/api/auth.ts
@@ -1,4 +1,10 @@
 // import { Google, type GoogleRefreshedTokens, type GoogleTokens } from "arctic";
+import type { Context } from "hono";
+import config from "@/config"
+import { db } from '@/db/client'
+import { getUserByEmail } from "@/db/user";
+import { getWorkspaceByEmail } from "@/db/workspace";
+const { JwtPayloadKey } = config
 
 // import { generateCodeVerifier, generateState } from "arctic";
 
@@ -18,3 +24,16 @@
 // const tokens: GoogleTokens = await google.validateAuthorizationCode(code, codeVerifier);
 // const refreshTokens: GoogleRefreshedTokens = await google.refreshAccessToken(refreshToken);
 
+export const GetUserWorkspaceInfo = async (c: Context) => {
+    const { sub } = c.get(JwtPayloadKey)
+    const email = sub
+    const workspace = await getWorkspaceByEmail(db, email)
+    if (!workspace) {
+        return c.json({ error: "Workspace not found" }, 404)
+    }
+    const user = await getUserByEmail(db, email)
+    if (!user || !user.length) {
+        return c.json({ error: "User not found" }, 404)
+    }
+    return c.json({user, workspace})
+}
