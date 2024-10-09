c8d0e3b99320513def40d0c2fdecc801405612ff
junaid <junaid.s@xynehq.com>
2024-10-09 12:13:41 +0530

    accept file path in eval script

--- server/eval/trec_eval.py
+++ server/eval/trec_eval.py
@@ -1,11 +1,28 @@
 import pytrec_eval
 import sys
+import argparse
 
 def main():
-    with open('data/output/dev_trec_qrels.tsv', 'r') as f_qrel:
+    # Argument parser to get file paths from the command line
+    parser = argparse.ArgumentParser(description='Evaluate TREC QREL results.')
+    parser.add_argument(
+        '--qrel_file', 
+        default='data/output/dev_trec_qrels.tsv', 
+        help='Path to the qrel file (default: data/output/dev_trec_qrels.tsv)'
+    )
+    parser.add_argument(
+        '--run_file', 
+        default='data/output/fiqa_result_qrels.tsv', 
+        help='Path to the run file (default: data/output/fiqa_result_qrels.tsv)'
+    )
+    
+    args = parser.parse_args()
+
+    # Open the provided file paths or use default
+    with open(args.qrel_file, 'r') as f_qrel:
         qrel = pytrec_eval.parse_qrel(f_qrel)
 
-    with open('data/output/fiqa_result_qrels.tsv', 'r') as f_run:
+    with open(args.run_file, 'r') as f_run:
         run = pytrec_eval.parse_run(f_run)
 
     # Specify the measures you want to evaluate
