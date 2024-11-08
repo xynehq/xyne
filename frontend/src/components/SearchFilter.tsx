bfd2a9c32d4fbc6bca7b53665c328740a9cd728e
Sahebjot singh <saheb@xynehq.com>
2024-11-08 18:10:14 +0530

    remove type errors

--- frontend/src/components/SearchFilter.tsx
+++ frontend/src/components/SearchFilter.tsx
@@ -2,7 +2,6 @@ import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuLabel,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
   DropdownMenuSeparator,
