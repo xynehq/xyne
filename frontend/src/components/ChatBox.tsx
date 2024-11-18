cc5d49fd5254008087855d59a966ca6779827026
kalpadhwaryu <kalpadhwaryu1581@gmail.com>
2024-11-18 21:00:07 +0530

    Fix selected files UI

--- frontend/src/components/ChatBox.tsx
+++ frontend/src/components/ChatBox.tsx
@@ -33,44 +33,41 @@ export const ChatBox = ({
   return (
     <div className="flex flex-col w-full border rounded-[20px] sticky bottom-[20px] bg-white">
       <div className="relative flex-col items-center">
-        <div>
-          {/* File Staging Area */}
-          {/* todo fix the ui */}
-          {stagedFiles.length > 0 && (
-            <div className="flex">
-              {/* <h4>Selected Files:</h4> */}
-              <ul className="flex">
-                {stagedFiles.map((file, index) => (
-                  <li key={index} style={{ border: 2, borderColor: "black" }}>
-                    {file.name}
-                    <button onClick={() => handleFileRemove(index)}>
-                      Remove
-                    </button>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
+        {stagedFiles.length > 0 && (
+          <div className="flex">
+            <ul className="flex">
+              {stagedFiles.map((file, index) => (
+                <li key={index} style={{ border: 2, borderColor: "black" }}>
+                  {file.name}
+                  <button onClick={() => handleFileRemove(index)}>
+                    Remove
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <div className="flex">
+          <textarea
+            ref={inputRef}
+            rows={1}
+            placeholder="Ask anything across apps..."
+            value={query}
+            className="flex-grow resize-none bg-transparent outline-none text-[15px] font-[450] leading-[24px] text-[#1C1D1F] placeholder-[#ACBCCC] pl-[16px] pt-[14px] max-h-[108px] overflow-auto"
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !e.shiftKey) {
+                e.preventDefault()
+                handleSend(query)
+              }
+            }}
+            style={{
+              height: "auto",
+              minHeight: "40px",
+              maxHeight: "108px",
+            }}
+          />
         </div>
-        <textarea
-          ref={inputRef}
-          rows={1}
-          placeholder="Ask anything across apps..."
-          value={query}
-          className="flex-grow resize-none bg-transparent outline-none text-[15px] font-[450] leading-[24px] text-[#1C1D1F] placeholder-[#ACBCCC] pl-[16px] pt-[14px] max-h-[108px] overflow-auto"
-          onChange={(e) => setQuery(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter" && !e.shiftKey) {
-              e.preventDefault()
-              handleSend(query)
-            }
-          }}
-          style={{
-            height: "auto",
-            minHeight: "40px",
-            maxHeight: "108px",
-          }}
-        />
       </div>
       <div
         className="flex ml-[16px] mr-[6px] mb-[6px] items-center space-x-3 pt-2 cursor-text"
