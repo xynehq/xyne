5e823c0432451ba1cd21ec66ecc564ae5f31c3fa
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2024-11-18 17:04:27 +0530

    feat(dx-improve): made changes for running prod smoothly

--- server/logger/index.ts
+++ server/logger/index.ts
@@ -3,6 +3,10 @@ import { Subsystem } from "@/types"
 import type { MiddlewareHandler, Context, Next } from "hono"
 import { getPath } from "hono/utils/url"
 import { v4 as uuidv4 } from "uuid"
+import { existsSync, mkdirSync } from 'fs';
+import { resolve } from 'path';
+
+const logsFolderPath = resolve(process.cwd(), 'server','logs');
 
 const humanize = (times: string[]) => {
   const [delimiter, separator] = [",", "."]
@@ -23,6 +27,7 @@ const time = (start: number) => {
 
 export const getLogger = (loggerType: Subsystem) => {
   if (process.env.NODE_ENV === "production") {
+    checkLogsFolder()
     return pino({
       name: `${loggerType}`,
       transport: {
@@ -145,5 +150,16 @@ export const LogMiddleware = (loggerType: Subsystem): MiddlewareHandler => {
         "Request completed",
       )
     }
+  }
+}
+
+
+
+const checkLogsFolder = () => {
+  if (!existsSync(logsFolderPath)) {
+    mkdirSync(logsFolderPath, {recursive:true});
+    console.log('Logs folder created.');
+  } else {
+    console.log('Logs folder already exists.');
   }
 }
