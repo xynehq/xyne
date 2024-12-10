ace8fc8b95bdc63c2d595243b980d238b2bbf725
junaid-shirur <junaid.s@xynehq.com>
2024-12-11 00:55:19 +0530

    script to replace dims

---
+++ server/vespa/replaceDIMS.ts
@@ -0,0 +1,48 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const replaceDimensions = (
+  filePath: string, 
+  modelDimension: number
+): void => {
+  try {
+    const schemaContent = fs.readFileSync(filePath, 'utf8');
+    const replacedContent = schemaContent.replace(/v\[(?:DIMS|\d+)\]/g, `v[${modelDimension.toString()}]`);
+
+    // Write the modified content to the output file
+    fs.writeFileSync(filePath, replacedContent, 'utf8');
+
+    console.log(`Update model dimension size`);
+  } catch (error) {
+    console.error('Error processing Vespa schema file:', error);
+    process.exit(1);
+  }
+};
+
+const batchReplaceDimensions = (filePaths: Array<string>,modelDimension: number): void => {
+  filePaths.forEach(p => {
+    try {
+      replaceDimensions(
+        p, 
+        modelDimension
+      );
+    } catch (error) {
+      console.error(`Failed to process ${p}:`, error);
+    }
+  });
+};
+
+const args = process.argv.slice(2)
+const getDotSdpaths = (directory: string) => {
+    return fs.readdirSync(directory)
+        .filter(file => path.extname(file) === ".sd")
+        .map(file => path.join(directory, file));
+};
+
+const paths = getDotSdpaths("./schemas");
+
+console.log(paths); 
+
+// Uncomment to run
+batchReplaceDimensions(paths, parseInt(args[0]));
+// replaceDimensions("./schemas/file.sd",1234)
