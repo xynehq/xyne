5997b024c34b80b0b0141e21d20da2e2f5eb4307
junaid-shirur <junaid.s@xynehq.com>
2025-01-09 16:18:05 +0530

    add env values for tests

---
+++ server/tests/setup.ts
@@ -0,0 +1,5 @@
+// env values for test
+// fake base-64 encoded keys
+process.env.ENCRYPTION_KEY = "9UGrx3uGR/ObEI+H1sG2hJGsuL2xeCvGKbBYPEsLJ6E=";
+process.env.SERVICE_ACCOUNT_ENCRYPTION_KEY = "9UGrx3uGR/ObEI+H1sG2hJGsuL2xeCvGKbBYPEsLJ6E="
+
