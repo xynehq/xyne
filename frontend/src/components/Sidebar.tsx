43063d468a64612efb3415077300e7639db352a3
Sahebjot singh <saheb@xynehq.com>
2025-01-28 02:06:42 +0530

    fix(styles): fix sidebar icon shift, link logo and change tooltip (#274)

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -22,10 +22,11 @@ export const Sidebar = ({
 }) => {
   const location = useLocation()
   const [showHistory, setShowHistory] = useState<boolean>(false)
+
   return (
     <TooltipProvider>
       <div
-        className={`bg-white h-full w-[52px] border-r-[0.5px] border-[#D7E0E9] flex flex-col items-center space-y-[10px] fixed ${className} z-20 select-none`}
+        className={`bg-white h-full w-[52px] border-r-[0.5px] border-[#D7E0E9] flex flex-col fixed ${className} z-20 select-none`}
       >
         {showHistory && (
           <HistoryModal
@@ -33,54 +34,67 @@ export const Sidebar = ({
             onClose={() => setShowHistory(false)}
           />
         )}
-        {photoLink && (
-          <img
-            className="w-[32px] h-[32px] mt-[16px] rounded-full mb-[15px]"
-            src={`/api/v1/proxy/${encodeURIComponent(photoLink)}`}
-            alt="Profile"
-          />
-        )}
-        <Link
-          to="/"
-          className="flex w-[32px] h-[32px] border-[1px] border-[#C4D0DC] items-center justify-center rounded-[6px]"
-        >
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Plus size={18} stroke="#384049" />
-            </TooltipTrigger>
-            <Tip side="right" info="Ask & Search" />
-          </Tooltip>
-        </Link>
-        <div
-          onClick={() => setShowHistory((history) => !history)}
-          className={`flex w-[32px] h-[32px] ${showHistory ? "bg-[#D8DFE680]" : ""} rounded-[8px] items-center justify-center cursor-pointer  hover:bg-[#D8DFE680]`}
-        >
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <History size={18} stroke="#384049" />
-            </TooltipTrigger>
-            <Tip side="right" info="History" />
-          </Tooltip>
+        <div className="flex flex-col items-center pt-4">
+          {photoLink && (
+            <img
+              className="w-8 h-8 rounded-full mb-4"
+              src={`/api/v1/proxy/${encodeURIComponent(photoLink)}`}
+              alt="Profile"
+            />
+          )}
         </div>
-        <Link
-          to={`${role === UserRole.SuperAdmin || role === UserRole.Admin ? "/admin/integrations" : "/integrations"}`}
-          className="flex w-[32px] h-[32px] items-center justify-center hover:bg-[#D8DFE680] rounded-[6px]"
+
+        <div className="flex flex-col items-center mt-[10px]">
+          <Link
+            to="/"
+            className="flex w-8 h-8 border border-[#C4D0DC] items-center justify-center rounded-md"
+          >
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Plus size={18} stroke="#384049" />
+              </TooltipTrigger>
+              <Tip side="right" info="New" />
+            </Tooltip>
+          </Link>
+
+          <div
+            onClick={() => setShowHistory((history) => !history)}
+            className={`flex w-8 h-8 ${showHistory ? "bg-[#D8DFE680]" : ""} rounded-lg items-center justify-center cursor-pointer hover:bg-[#D8DFE680] mt-[10px]`}
+          >
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <History size={18} stroke="#384049" />
+              </TooltipTrigger>
+              <Tip side="right" info="History" />
+            </Tooltip>
+          </div>
+
+          <Link
+            to={`${role === UserRole.SuperAdmin || role === UserRole.Admin ? "/admin/integrations" : "/integrations"}`}
+            className="flex w-8 h-8 items-center justify-center hover:bg-[#D8DFE680] rounded-md mt-[10px]"
+          >
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Plug
+                  stroke="#384049"
+                  size={18}
+                  {...(location.pathname === "/admin/integrations" ||
+                  location.pathname === "/integrations"
+                    ? { className: "text-blue-500 hover:text-blue-600" }
+                    : { className: "hover:text-blue-600" })}
+                />
+              </TooltipTrigger>
+              <Tip side="right" info="Integrations" />
+            </Tooltip>
+          </Link>
+        </div>
+
+        <a
+          href="https://xynehq.com"
+          className="mt-auto mb-4 flex justify-center"
         >
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Plug
-                stroke="#384049"
-                size={18}
-                {...(location.pathname === "/admin/integrations" ||
-                location.pathname === "/integrations"
-                  ? { className: "text-blue-500 hover:text-blue-600" }
-                  : { className: "hover:text-blue-600" })}
-              />
-            </TooltipTrigger>
-            <Tip side="right" info="Integrations" />
-          </Tooltip>
-        </Link>
-        <img style={{ marginTop: "auto", marginBottom: "16px" }} src={Logo} />
+          <img src={Logo} alt="Logo" />
+        </a>
       </div>
     </TooltipProvider>
   )
