0baf0b0d18b100f8225d8394eb3835b910b23901
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2025-03-27 14:32:58 +0530

    feat(init-script-fix): Fixed issues in init-script and removed .env.development

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,15 +1,5 @@
 import config from "@/config"
 import { defineConfig } from "drizzle-kit"
-import dotenv from 'dotenv';
-
-const envFiles = ['.env.development', `.env.${process.env.NODE_ENV}`, '.env', ];
-
-for (const file of envFiles) {
-  if (require('fs').existsSync(file)) {
-    dotenv.config({ path: file });
-    break;
-  }
-}
 
 export default defineConfig({
   dialect: "postgresql",
