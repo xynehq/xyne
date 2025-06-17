19bd86d892a45665a486cbe8b545d81f80e787a1
Nasim Sheikh <nasim.sheikh@Nasim-Sheikh-G7XLW4719J.local>
2025-06-17 15:09:43 +0530

    Added script to ingest google data

--- server/integrations/google/config.ts
+++ server/integrations/google/config.ts
@@ -21,5 +21,5 @@ export const GmailConcurrency = 8
 export const PDFProcessingConcurrency = 8
 
 export const MAX_ATTACHMENT_PDF_SIZE = 15
-// We will skip the Vespa existence validation step and ingest emails directly.
-export const skipMailExistCheck = true
+// We will skip the Vespa existence validation step and ingest emails directly if below flag is true.
+export const skipMailExistCheck = false
---
+++ server/scripts/googleConfig.ts
@@ -0,0 +1,6 @@
+    export const serviceAccountConnectorId = 288 // need to change to point to service account connectorId
+    export const  WHITELISTED_EMAILS = "junaid.s@xynehq.com,saheb@xynehq.com"
+    export const ingestDrive = true
+    export const ingestMail = true
+    export const ingestEvent = true
+    
