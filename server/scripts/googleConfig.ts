8f69ee8c2d2a744b33470dc4b108f1625e410456
Nasim Sheikh <nasim.sheikh@Nasim-Sheikh-G7XLW4719J.local>
2025-06-17 15:17:25 +0530

    removed hardCoded emails

--- server/scripts/googleConfig.ts
+++ server/scripts/googleConfig.ts
@@ -1,6 +1,5 @@
     export const serviceAccountConnectorId = 288 // need to change to point to service account connectorId
-    export const  WHITELISTED_EMAILS = "junaid.s@xynehq.com,saheb@xynehq.com"
+    export const  WHITELISTED_EMAILS = "email@TobeIngested"
     export const ingestDrive = true
     export const ingestMail = true
     export const ingestEvent = true
-    
