8534353fc625213a2b2f258b62e30804c7ccb5eb
Himansh varma <126441540+Himanshvarma@users.noreply.github.com>
2025-06-28 15:16:18 +0530

    feat(proxy): added vespa search proxy (#615)
    * added vespa search proxy
    
    * solved ai comments
    
    * added user secret and time for api key
    
    * moved the apikey html to frontend
    
    * fixed dropdown menu

--- server/config.ts
+++ server/config.ts
@@ -2,8 +2,8 @@ import { isURLValid } from "@/validate"
 import { Models } from "@/ai/types"
 let vespaBaseHost = "0.0.0.0"
 let postgresBaseHost = "0.0.0.0"
-let port = 3000
-let host = "http://localhost:3000"
+let port = process.env.PORT || 3000
+let host = process.env.HOST || "http://localhost:3000"
 let redirectUri = process.env.GOOGLE_REDIRECT_URI!
 let postOauthRedirect = "/"
 if (process.env.NODE_ENV === "production") {
