217e2a1028526c22d736333c622778323bff88c7
Oindrila Banerjee <banerjee.oindrila.ob@gmail.com>
2025-09-05 18:17:21 +0530

    fix(test): testing semantic release
    testing semantic release

--- server/init-vespa.sh
+++ server/init-vespa.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "Setting up permissions for vespa in docker...."
 mkdir -p ./vespa-data ./vespa-logs
 sudo chown -R 1000:1000 ./vespa-data ./vespa-logs
 sudo chmod -R 755 ./vespa-data ./vespa-logs
 echo "Successfully set permissions for vespa in docker...."
+
