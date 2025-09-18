c62c36adadb3b91997ffc4ec5a1a8243683f2acb
Oindrila Banerjee <53270901+oindrila-b@users.noreply.github.com>
2025-09-18 20:39:13 +0530

    fix: XYN-288 Added changes for release fix (#886)
    * fix: XYN-288 Added changes for release fix
    
    * fix: XYN-288 Release script
    
    * fix: XYN-288 Modifed release.yml
    
    * fix: XYN-288 Modified release.yml
    
    * fix: XYN-288 MODIFIED RELEASE FILE
    
    * chore(release): 1.0.0 [skip ci]
    
    ---------
    
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>

--- server/release.config.js
+++ server/release.config.js
@@ -1,6 +1,6 @@
 // release.config.js
 export default {
-  branches: ['main'],
+  branches: ['main', 'release_test_sim', 'fix/release_file'],
   plugins: [
     '@semantic-release/commit-analyzer', // decides bump (major/minor/patch)
     '@semantic-release/release-notes-generator', // generates human-readable notes
