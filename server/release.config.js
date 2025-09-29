0bf3efa70c248e5a5e0eb83b6fc3a5b6163e7591
Oindrila Banerjee <banerjee.oindrila.ob@gmail.com>
2025-09-29 13:29:47 +0530

    feat: XYN-155 modifed release.config.js
    BREAKING CHANGE: Testing release

--- server/release.config.js
+++ server/release.config.js
@@ -1,6 +1,6 @@
 // release.config.js
 export default {
-  branches: ['main', 'release_test_pat_sim'], // release from main and release_test branches
+  branches: ['main', 'release_test_pat_sim', 'release_test_pat'], // release from main and release_test branches
   plugins: [
     '@semantic-release/commit-analyzer', // decides bump (major/minor/patch)
     '@semantic-release/release-notes-generator', // generates human-readable notes
