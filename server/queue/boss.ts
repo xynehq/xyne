f7fb28d5e15199fdcb57096d22d33c0b42f44104
Shivam Ashtikar <shivam.ashtikar@juspay.in>
2025-09-30 12:59:36 +0530

    fix: XYN-302 addressed bot comments

--- server/queue/boss.ts
+++ server/queue/boss.ts
@@ -4,5 +4,5 @@ import config from "@/config"
 const url = `postgres://xyne:xyne@${config.postgresBaseHost}:5432/xyne`
 export const boss = new PgBoss({
   connectionString: url,
-  monitorStateIntervalMinutes: 10, // Monitor state every minute
+  monitorStateIntervalMinutes: 10, // Monitor state every 10 minutes
 })
