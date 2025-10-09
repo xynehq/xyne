e9b19022c9d965c62444e71aadbad71ddc9249df
Oindrila Banerjee <banerjee.oindrila.ob@gmail.com>
2025-10-09 16:16:34 +0530

    fix: XYNE-97 Modified files for changelog files

--- server/release.config.js
+++ server/release.config.js
@@ -1,39 +1,25 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 export default {
   branches: [
     "main",
-     { name: "fix/changelog", prerelease: "changelog" },
-
-    {
-      name: "release-*",
-      prerelease: true
-    },
+    { name: "fix/changelog", prerelease: "changelog" },
+    { name: "release-*", prerelease: true },
   ],
   plugins: [
-    "@semantic-release/commit-analyzer", // decides bump (major/minor/patch)
-    "@semantic-release/release-notes-generator", // generates human-readable notes
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
     [
-      "@semantic-release/changelog", // writes to CHANGELOG.md
+      "@semantic-release/changelog",
       {
-        changelogFile: path.resolve(__dirname, "../CHANGELOG.md"),
+        changelogFile: "CHANGELOG.md",
       },
     ],
-    [
-      "@semantic-release/npm",
-      {
-        npmPublish: false, // no npm publish
-      },
-    ],
+    ["@semantic-release/npm", { npmPublish: false }],
     [
       "@semantic-release/git",
       {
-        assets: ["package.json",  path.resolve(__dirname, "../CHANGELOG.md"),], // commit changelog + version
-        message: "chore(release): ${nextRelease.version} [skip ci] n\n${nextRelease.notes}",
+        assets: ["package.json", "CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
   ],
