0139018d3bcc5eb56a2bb58d8d6f5103ad210e68
Oindrila Banerjee <banerjee.oindrila.ob@gmail.com>
2025-10-09 12:45:46 +0530

    fix: XYN-97 Modified files for generating changelog

--- server/release.config.js
+++ server/release.config.js
@@ -5,7 +5,8 @@ export default {
     {
       name: "release-*",
       prerelease: true
-    }
+    },
+    { name: "fix/changelog", prerelease: true },
   ],
   plugins: [
     "@semantic-release/commit-analyzer", // decides bump (major/minor/patch)
@@ -13,7 +14,7 @@ export default {
     [
       "@semantic-release/changelog", // writes to CHANGELOG.md
       {
-        changelogFile: "../CHANGELOG.md",
+        changelogFile: path.resolve(__dirname, "../CHANGELOG.md"),
       },
     ],
     [
@@ -25,8 +26,8 @@ export default {
     [
       "@semantic-release/git",
       {
-        assets: ["package.json", "../CHANGELOG.md"], // commit changelog + version
-        message: "chore(release): ${nextRelease.version} [skip ci]",
+        assets: ["package.json",  path.resolve(__dirname, "../CHANGELOG.md"),], // commit changelog + version
+        message: "chore(release): ${nextRelease.version} [skip ci] n\n${nextRelease.notes}",
       },
     ],
   ],
