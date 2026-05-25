daece0feffdabb4276265f1de16be98990dce630
Junaid <88700111+junaid-shirur@users.noreply.github.com>
2026-05-25 14:59:36 +0530

    fix(db): use tx db instance within transactions

--- server/api/chat/chat.ts
+++ server/api/chat/chat.ts
@@ -5331,7 +5331,7 @@ export const MessageApi = async (c: Context) => {
           // we are updating the chat and getting it's value in one call itself
 
           let existingChat = await updateChatByExternalIdWithAuth(
-            db,
+            tx,
             chatId,
             email,
             {},
